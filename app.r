8c2e5029168edc3c50af826f4458f474ea183f62
Dipro Mondal <50530569+DiproMondal@users.noreply.github.com>
2023-11-10 09:16:35 +0100

    app.r
    Signed-off-by: Dipro Mondal <50530569+DiproMondal@users.noreply.github.com>

--- app.r
+++ app.r
@@ -699,7 +699,7 @@ shinyApp(
                                            conditionalPanel(
                                              condition = "input.SSizeProc == 'Width of Confidence Interval Approach'",
                                              list(sliderInput("targetwidth", label="Target Width", min = 0.05, max = 1, step = 0.05, value=0.2),
-                                                  sliderInput("targetICC", label="Value of ICC", min = 1e-6, max = 0.99, step = 0.01, value=0.8),
+                                                  sliderInput("targetICC", label="Value of ICC", min = 0.01, max = 0.99, step = 0.01, value=0.8),
                                                   numericInput("nsims", label = "Maximum value of n:",min=5000, max = 5e7, step=2500, value= 5000)
                                              )
                                            ),
@@ -707,15 +707,15 @@ shinyApp(
                                              condition = "input.SSizeProc == 'Assurance Probability Approach'",
                                              list(sliderInput("assurance", label="Assurance Probability", min = 0.5, max = 0.999, step = 0.05, value=0.5),
                                                   sliderInput("targetwidthA", label="Target Width", min = 0.05, max = 1.0, step = 0.025, value=0.2),
-                                                  sliderInput("targetICCA", label="Value of ICC", min = 1e-6, max = 0.99, step = 0.01, value=0.9),
+                                                  sliderInput("targetICCA", label="Value of ICC", min = 0.01, max = 0.99, step = 0.01, value=0.9),
                                                   numericInput("nmaxA", label = "Maximum value of n:",min=5000, max = 5e7, step=2500, value= 5000)
                                              )
                                            ),
                                            conditionalPanel(
                                              condition = "input.SSizeProc == 'Testing Approach'",
                                              list(sliderInput("power", label="Power of Hyothesis Test", min = 0.5, max = 0.999, step = 0.05, value=0.9),
-                                                  sliderInput("ICCNull", label="ICC value at null hypothesis", min = 0.0, max = 0.999, step = 0.025, value=0.8),
-                                                  sliderInput("ICCAlt", label="ICC value at alternative hypothesis", min = 0.001, max = 0.999, step = 0.025, value=0.9),
+                                                  sliderInput("ICCNull", label="ICC value at null hypothesis", min = 0.0, max = 0.99, step = 0.01, value=0.8),
+                                                  sliderInput("ICCAlt", label="ICC value at alternative hypothesis", min = 0.01, max = 0.99, step = 0.01, value=0.9),
                                                   numericInput("nmaxT", label = "Maximum value of n:",min=5000, max = 5e7, step=2500, value= 5000)
                                              )
                                            ),
